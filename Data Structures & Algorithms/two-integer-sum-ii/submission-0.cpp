ee2c48b33d7e3d4786b3e4d8fc68ef40cab258ce
joshualin40 <joshualin40@gmail.com>
2026-04-19 10:13:05 -0400

    Add: two-integer-sum-ii - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.cpp
@@ -0,0 +1,14 @@
+// try in O(n^2) time first 
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        for (int i =0; i < numbers.size(); i++)
+        {
+            for (int j = i + 1; j < numbers.size(); j++)
+            {
+                if (numbers[i] + numbers[j] == target) return {i+1,j+1};
+            }
+        }  
+    }
+};
