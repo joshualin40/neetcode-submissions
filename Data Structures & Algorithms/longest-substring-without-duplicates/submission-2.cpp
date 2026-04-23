c64fee7f302bf3e1c0420ca08a71c1f86719d75a
joshualin40 <joshualin40@gmail.com>
2026-04-23 11:45:56 -0400

    Add: longest-substring-without-duplicates - submission-2

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-2.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        if (s == "") return 0;
+        int left = 0;
+        int right = 1;
+        while (right < s.length())
+        {
+            bool hasdup = false;
+            string substring = s.substr(left,right-left+1);
+            unordered_map<char, int> set1; 
+            for (int i = 0; i < substring.length(); i++)
+                set1[substring[i]]++; 
+            for (const auto& [key,value] : set1)
+            {
+                if (value > 1) hasdup = true;
+            }
+            if (hasdup) 
+            {
+                left++;
+                right++;
+            }
+            else
+            {
+                right++;
+            }
+        }
+        right--;
+        return right - left + 1; 
+    }
+};
