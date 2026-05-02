06b371a01b8dfa7e5f45987e701c130202676cc4
joshualin40 <joshualin40@gmail.com>
2026-05-02 11:47:09 -0400

    Add: k-closest-points-to-origin - submission-5

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-5.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        // use a minHeap that only maintains K values at each given time
+        // store the min heap full of int pairs for dist/index
+        // sort of by dist
+        // when finished iterating through points vector, maintaining only K 
+        // elements in the heap, iterate through the minheap adding the indices to 
+        // res vector 
+        priority_queue<pair<double,int>> maxHeap;
+        vector<vector<int>> res;
+        for (int i = 0; i < points.size(); i++)
+        {
+            double dist = double(pow(points[i][0]*points[i][0] + points[i][1]*points[i][1], 2));
+            if (maxHeap.size() < k)
+                maxHeap.push({dist, i});
+            else if (maxHeap.size() == k && dist < maxHeap.top().first)
+            {
+                maxHeap.pop();
+                maxHeap.push({dist, i});
+            }
+        } 
+        for (int i = 0; i < k; i++)
+        {
+            res.push_back(points[maxHeap.top().second]);
+            maxHeap.pop();
+        }
+
+        return res;
+    }
+};
