2f61dbfa284a95980b12890e50f50e09ef8f1546
Joshua Lin <joshualin40@gmail.com>
2026-08-20 11:09:50 -0400

    Add: flood-fill - submission-0

---
+++ Data Structures & Algorithms/flood-fill/submission-0.py
@@ -0,0 +1,27 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        # modify the grid in-place
+        ROWS, COLS = len(image), len(image[0])
+
+        startcolor = image[sr][sc]
+        if (startcolor == color):
+            return image
+        def dfs(r, c):
+            if (min(r,c) < 0 or r == ROWS or c == COLS or image[r][c] != startcolor):
+                return
+            
+            image[r][c] = color
+
+            dfs(r+1, c)
+            dfs(r-1, c)
+            dfs(r, c+1)
+            dfs(r, c-1)
+            
+        dfs(sr,sc)
+        return image
+
+
+
+            
+
+        
